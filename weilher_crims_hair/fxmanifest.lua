b2c9721ea55f36a635431223ba4bedbd35ab0f3d
Weilher <82490121+Weilher@users.noreply.github.com>
2023-08-20 22:54:20 +0200

    Add files via upload

---
+++ weilher_crims_hair/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version "cerulean"
+games { "gta5" }
+
+author "Weilher420"
+description "Crim's Hair for Nova RolePlay (Retexture of the Hawick Avenue Barber)"
+version "1.0"
+
+this_is_a_map "yes"
+
+-- Location : -30.08, -141.59, 56.04
